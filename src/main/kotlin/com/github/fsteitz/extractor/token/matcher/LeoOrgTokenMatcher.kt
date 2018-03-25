e96b81fe9d5932913caa38eabf9abbda3225341c
Florian Steitz <florian@fsteitz.com>
2018-03-25 10:54:09 +0200

    LeoOrgTokenMatcher can now handle more URL variations

--- src/main/kotlin/com/github/fsteitz/extractor/token/matcher/LeoOrgTokenMatcher.kt
+++ src/main/kotlin/com/github/fsteitz/extractor/token/matcher/LeoOrgTokenMatcher.kt
@@ -19,7 +19,7 @@ package com.github.fsteitz.extractor.token.matcher
 import java.nio.charset.Charset
 
 private const val DOMAIN_PATTERN = """(?:pda|dict)\.leo\.org\/"""
-private const val QUERY_PATTERN = """[a-zA-Z=0-9]*(?:&[a-zA-Z=0-8]*)*(?:search=([^&]*)).*?"""
+private const val QUERY_PATTERN = """[a-zA-Z=0-9_]*(?:&[a-zA-Z=0-9_]*)*(?:search=([^&]*)).*?"""
 
 /**
  * @author Florian Steitz (florian@fsteitz.com)
@@ -29,9 +29,21 @@ class LeoOrgTokenMatcher(charset: Charset) : HttpTokenMatcher(charset) {
   override val domainPattern = DOMAIN_PATTERN
   override val pathPatterns by lazy {
     listOf(
+            // Example: http://dict.leo.org/ende?lp=ende&lang=en&searchLoc=0&cmpType=relaxed&sectHdr=on&spellToler=&search=VOCAB
+            // Example: http://dict.leo.org/ende?lp=ende&p=_xpAA&search=VOCAB
             """ende\?$QUERY_PATTERN""",
+
+            // Example: http://dict.leo.org/dictQuery/m-vocab/ende/de.html?searchLoc=0&lp=ende&lang=de&search=VOCAB&resultOrder=basic
             """dictQuery\/m-vocab\/ende\/de\.html\?$QUERY_PATTERN""",
+
+            // Example: https://dict.leo.org/ende/index_de.html#/search=VOCAB&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&pos=0
+            """ende\/index_de\.html#\/$QUERY_PATTERN""",
+
+            // Example: http://dict.leo.org/#/search=VOCAB&searchLoc=0&resultOrder=basic&multiwordShowSingle=on
             """#\/$QUERY_PATTERN""",
+
+            // Example: https://dict.leo.org/englisch-deutsch/VOCAB
+            // Example: http://pda.leo.org/englisch-deutsch/VOCAB
             """englisch-deutsch\/([^&]*)"""
     )
   }
